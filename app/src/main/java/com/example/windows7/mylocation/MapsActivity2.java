d1d271b2cc7e11206b4c4253aa793d4086b30d7b
Vanessa Esther Pacheco De La Rosa <t00036318@utbvirtual.edu.co>
2016-09-25 22:46:19 -0500

    Parte 2

---
+++ app/src/main/java/com/example/windows7/mylocation/MapsActivity2.java
@@ -0,0 +1,108 @@
+package com.example.windows7.mylocation;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.windows7.mylocation.R;
+import com.example.windows7.mylocation.app.AppController;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MapsActivity2 extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    String urlJsonObj ="http://labsoftware03.unitecnologica.edu.co/archivoNicolas";
+
+    private static String TAG = MapsActivity2.class.getSimpleName();
+
+    private String jsonResponse;
+
+    double lat = 0.0;
+    double lng = 0.0;
+    private TextView latlng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps2);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        latlng = (TextView) findViewById(R.id.latlng);
+        makeJsonObjectRequest();
+    }
+
+    private void makeJsonObjectRequest() {
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+                urlJsonObj, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    // Parsing json object response
+                    // response will be a json object
+                    lat = response.getDouble("latitud");
+                    lng = response.getDouble("longitud");
+
+                    jsonResponse = "";
+                    jsonResponse += "Latitude: " + lat + "\n\n";
+                    jsonResponse += "Longitude: " + lng + "\n\n";
+
+                    latlng.setText(jsonResponse);
+                    onMapReady(mMap);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(),
+                            "Error: " + e.getMessage(),
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng location = new LatLng(lat, lng);
+        mMap.addMarker(new MarkerOptions().position(location).title("Location from Web Service"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15));
+    }
+}
